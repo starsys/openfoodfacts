62bcc41bf1da8168b9aec5b5a98478a19cbecee0
Starsys <theophile.guettier@gmail.com>
2019-05-28 17:52:18 +0200

    Update of terminal interface in main. Need code refacturing to improve readability

--- main.py
+++ main.py
@@ -19,63 +19,66 @@ from pprint import *
 
 
 def print_sql():
-    print("Ce programme vous propose de substituer un aliment par un équivalent plus sain. Voici les catégories que vous pouvez parcourir :")
+    print("Ce programme vous propose de substituer un aliment par un équivalent plus sain. Voici les catégories que vous pouvez parcourir : \n")
     id_category = []
     id_product = []
     id_substituted = []
     for row in make_query("SELECT * FROM category; SELECT %s", ("", )):
         print("ID:", row[0], "|", row[1])
         id_category.append(row[0])
+    print("")
     choice = input("Veuillez choisir une catégorie en saisissant son ID puis entrée : ")
-    print("\n")
+    print("-------------------------------------------------------------------------------------------------------")
     while not choice.isdigit() or int(choice) not in id_category:
-        print("Veuillez saisir un identifiant existant")
-        print("\n")
+        print("-------------------------------------------------------------------------------------------------------")
+        print("Veuillez saisir un identifiant existant ! \n")
         for row in make_query("SELECT * FROM category; SELECT %s", ("",)):
             print("ID:", row[0], "|", row[1])
+        print("")
         choice = input("Veuillez choisir une catégorie en saisissant son ID puis entrée : ")
-        print("\n")
     category_name = make_query("SELECT name FROM category WHERE id = %s", (choice,))[0][0]
+    print("")
+    print("-------------------------------------------------------------------------------------------------------")
     for row in make_query("SELECT * FROM product WHERE category_ID = %s", (choice,)):
         print(row[0], "|", row[1])
         id_product.append(row[0])
+    print("")
     choice2 = input("Veuillez choisir un produit à substituer en saisissant son ID puis entrée : ")
-    print("\n")
     while not choice2.isdigit() or int(choice2) not in id_product:
-        print("Veuillez saisir un identifiant existant")
-        print("\n")
+        print("-------------------------------------------------------------------------------------------------------")
+        print("Veuillez saisir un identifiant existant ! \n")
         for row in make_query("SELECT * FROM product WHERE category_ID = %s", (choice,)):
             print("ID:", row[0], "|", row[1])
-        choice2 = input("Veuillez choisir un produit à substituer en saisissant son identifiant puis entrée : ")
-        print("\n")
+        print("")
+        choice2 = input("Veuillez choisir un produit à substituer en saisissant son ID puis entrée : ")
+    print("-------------------------------------------------------------------------------------------------------")
     nutrition_grade_selected_product = make_query("SELECT nutrition_grades FROM product WHERE id = %s", (choice2,))[0][
         0]
     product_name = make_query("SELECT name FROM product WHERE id = %s", (choice2,))[0][0]
     # print(nutrition_grade_selected_product)
-    print("\n")
     if not make_query("SELECT * FROM product WHERE nutrition_grades < %s AND category_ID = %s",
                           (nutrition_grade_selected_product, choice)):
-        print("Il n'existe pas de produit plus sain que ", product_name, " dans la catégorie", category_name)
+        print("Il n'existe pas de produit plus sain que", product_name, " dans la catégorie", category_name)
     else:
         print("Vous pourriez remplacer cet aliment par : \n")
         for row in make_query("SELECT * FROM product WHERE nutrition_grades < %s AND category_ID = %s",
                               (nutrition_grade_selected_product, choice)):
             print("ID:", row[0], "|", row[1], "|", "Description : ", row[5], "|", row[4], "|", row[6])
             id_substituted.append(row[0])
-        print("\n")
+        print("")
         choice3 = input("Si vous souhaitez enregistrer l'un de ces produit en substitution de " + product_name + ", tapez son ID, sinon, tapez entrée pour quitter : ")
-        print("\n")
+        while choice3 and (not choice3.isdigit() or int(choice3) not in id_substituted):
+            print("Veuillez saisir un identifiant existant ! \n")
+            for row in make_query("SELECT * FROM product WHERE nutrition_grades < %s AND category_ID = %s",
+                                  (nutrition_grade_selected_product, choice)):
+                print("ID:", row[0], "|", row[1], "|", "Description : ", row[5], "|", row[4], "|", row[6])
+            print("")
+            choice3 = input("Si vous souhaitez enregistrer l'un de ces produit en substitution de " + product_name + ", tapez son ID, sinon, tapez entrée pour quitter : ")
         if not choice3:
             print("Au revoir")
         else:
-            while not choice3.isdigit() or int(choice3) not in id_substituted:
-                print("Veuillez saisir un identifiant existant")
-                print("\n")
-                for row in make_query("SELECT * FROM product WHERE nutrition_grades < %s AND category_ID = %s",
-                                      (nutrition_grade_selected_product, choice)):
-                    print("ID:", row[0], "|", row[1], "|", "Description : ", row[5], "|", row[4], "|", row[6])
-                choice3 = input("Si vous souhaitez enregistrer l'un de ces produit en substitution de " + product_name + ", tapez son ID, sinon, tapez entrée pour quitter : ")
-            print("\n")
+            print("")
+            print("-------------------------------------------------------------------------------------------------------")
             substituted_name = make_query("SELECT name FROM product WHERE id = %s", (choice3,))[0][0]
             print(substituted_name)
 
